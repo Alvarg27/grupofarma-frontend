f0fe3421faae0f8fae722aee0597b42f22acd88c
Alvarg27 <alvargbesne@gmail.com>
2022-08-09 00:41:38 -0500

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -4,6 +4,7 @@ import client from "../lib/client";
 import Image from "next/image";
 import useTemplate from "../hooks/useTemplate";
 import Icon from "./Icon";
+import Link from "next/link";
 
 export default function Footer({ contact, siteSettings }) {
   const logoImage = siteSettings[0].logoImage;
@@ -17,52 +18,63 @@ export default function Footer({ contact, siteSettings }) {
   return (
     <div
       style={{ background: template.secondaryColor }}
-      className="flex flex-col mt-[150px]"
+      className="flex flex-col mt-[100px]"
     >
-      <div className="-mt-[150px] w-[70%] mx-auto">
-        <Image
-          src={urlFor(contact[0].footerImage).width(850).url()}
-          objectFit="contain"
-          layout="responsive"
-          height={100}
-          width={100}
-          priority={true}
-          placeholder="blur"
-          blurDataURL={urlFor(contact[0].footerImage).width(10).url()}
-        />
-      </div>
-      <div className="mx-auto">
-        <Image
-          src={urlFor(logoImage).width(400).url()}
-          layout="fixed"
-          objectFit="contain"
-          height={30}
-          width={180}
-          priority={true}
-        />
-      </div>
-      <h1
-        style={{ color: template.textColor }}
-        className="text-4xl font-bold text-center lg:text-6xl"
-      >
-        {title}
-      </h1>
-      <div className="flex m-auto my-4">
-        {contact[0].socialMedia.map((item) => (
-          <div key={item._key}>
-            <Icon name={item.icon.name} className="m-2 text-4xl" />
+      <div className="lg:flex lg:justify-between px-24">
+        <div className="-mt-[100px] w-[70%] mx-auto lg:w-[400px] lg:mx-0">
+          <Image
+            src={urlFor(contact[0].footerImage).width(850).url()}
+            objectFit="contain"
+            layout="responsive"
+            height={100}
+            width={100}
+            priority={true}
+            placeholder="blur"
+            blurDataURL={urlFor(contact[0].footerImage).width(10).url()}
+          />
+        </div>
+        <div className="flex flex-col mt-8">
+          <div className="mx-auto lg:mx-0">
+            <Image
+              src={urlFor(logoImage).width(400).url()}
+              layout="fixed"
+              objectFit="contain"
+              height={30}
+              width={180}
+              priority={true}
+            />
           </div>
-        ))}
-      </div>
-      <h3 className="font-semibold text-center mx-auto">
-        {contact[0].mainEmail}
-      </h3>
-      <div>
-        {contact[0].phoneNumbers.map((item) => (
-          <h3 key={item._key} className=" text-center mx-auto">
-            {item}
+          <h1
+            style={{ color: template.textColor }}
+            className="text-4xl font-bold text-center mx-auto lg:mx-0 lg:text-left lg:text-6xl"
+          >
+            {title}
+          </h1>
+          <div className="flex m-auto my-4 lg:mx-0">
+            {contact[0].socialMedia.map((item) => (
+              <div key={item._key}>
+                <Link href={item.socialLink}>
+                  <a target="_blank">
+                    <Icon name={item.icon.name} className="m-2 text-4xl" />
+                  </a>
+                </Link>
+              </div>
+            ))}
+          </div>
+          <h3 className="font-semibold text-center mx-auto lg:mx-0 lg:text-left">
+            {contact[0].mainEmail}
           </h3>
-        ))}
+          <div>
+            {contact[0].phoneNumbers.map((item) => (
+              <h3
+                key={item._key}
+                className=" text-center mx-auto lg:mx-0 lg:text-left"
+              >
+                {item}
+              </h3>
+            ))}
+          </div>
+        </div>
       </div>
       <p className="my-6 mx-auto text-gray-500">
         powered by <b>tectify</b>
