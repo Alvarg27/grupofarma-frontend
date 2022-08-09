4cc949c99984826e68321f1c146e2119c979ea09
Alvarg27 <alvargbesne@gmail.com>
2022-08-08 23:27:41 -0500

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,28 +1,9 @@
 import "../styles/globals.css";
 import { TemplateProvider } from "../context/TemplateProvider";
-import Router from "next/router";
-import { useEffect, useState } from "react";
-import LoadingScreen from "../components/LoadingScreen";
 
 function MyApp({ Component, pageProps }) {
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    setTimeout(() => {
-      setLoading(false);
-    }, 1000);
-  }, []);
-
-  useEffect(() => {
-    if (loading) {
-      document.body.style.overflow = "hidden";
-    } else {
-      document.body.style.overflow = "scroll";
-    }
-  }, [loading]);
   return (
     <TemplateProvider>
-      {loading ? <LoadingScreen /> : ""}
       <Component {...pageProps} />
     </TemplateProvider>
   );
