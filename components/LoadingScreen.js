3bf8a8ff8ab59c795f3f206126b10933343a4d03
Alvarg27 <alvargbesne@gmail.com>
2022-08-08 23:24:57 -0500

    update

--- components/LoadingScreen.js
+++ components/LoadingScreen.js
@@ -2,9 +2,12 @@ import Image from "next/image";
 import React from "react";
 import icon from "../public/icon-gf.svg";
 
-export default function LoadingScreen() {
+export default function LoadingScreen({ loading }) {
   return (
-    <div className="absolute bg-white w-full h-screen z-50 flex ">
+    <div
+      style={{ opacity: loading ? 0 : 1 }}
+      className="fixed top-0 bg-white w-full h-screen z-50 flex "
+    >
       <div className="m-auto animate-pulse drop-shadow-lg">
         <Image src={icon} layout="fixed" height={80} />
       </div>
--- pages/_app.js
+++ pages/_app.js
@@ -5,28 +5,25 @@ import { useEffect, useState } from "react";
 import LoadingScreen from "../components/LoadingScreen";
 
 function MyApp({ Component, pageProps }) {
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(true);
+
   useEffect(() => {
-    const start = () => {
-      console.log("start");
-      setLoading(true);
-    };
-    const end = () => {
-      console.log("findished");
+    setTimeout(() => {
       setLoading(false);
-    };
-    Router.events.on("routeChangeStart", start);
-    Router.events.on("routeChangeComplete", end);
-    Router.events.on("routeChangeError", end);
-    return () => {
-      Router.events.off("routeChangeStart", start);
-      Router.events.off("routeChangeComplete", end);
-      Router.events.off("routeChangeError", end);
-    };
+    }, 1000);
   }, []);
+
+  useEffect(() => {
+    if (loading) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "scroll";
+    }
+  }, [loading]);
   return (
     <TemplateProvider>
-      {loading ? <LoadingScreen /> : <Component {...pageProps} />}
+      {loading ? <LoadingScreen /> : ""}
+      <Component {...pageProps} />
     </TemplateProvider>
   );
 }
